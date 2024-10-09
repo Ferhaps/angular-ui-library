84b0e32b35541bf7eb9338a91ef1c73dada53f7b
Ferhan Cherkez <fericherkez14052002@gmail.com>
2024-10-09 21:51:19 +0300

    showing correct error status code text

--- projects/ui-lib/src/lib/components/error-handler/error-popup/error-popup.component.ts
+++ projects/ui-lib/src/lib/components/error-handler/error-popup/error-popup.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpErrorResponse } from '@angular/common/http';
 import { DefaultDialogComponent } from '../../default-dialog/default-dialog.component';
 import { ErrorDispalyComponent } from '../../error-dispaly.component';
+import { HTTP_STATUS_CODES } from '../../../utills/utils';
 
 @Component({
   selector: 'lib-error-popup',
@@ -15,5 +16,7 @@ import { ErrorDispalyComponent } from '../../error-dispaly.component';
   styleUrls: ['./error-popup.component.scss'],
 })
 export class ErrorPopupComponent {
+  protected httpStatusCodes = HTTP_STATUS_CODES;
+  
   constructor(@Inject(MAT_DIALOG_DATA) public error: HttpErrorResponse) { }
 }
