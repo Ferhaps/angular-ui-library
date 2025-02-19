92ec7ddc359628e5f5d80da49c0ac170eb9cf90a
Ferhan Cherkez <fericherkez14052002@gmail.com>
2025-02-19 20:14:44 +0200

    next version

--- projects/ui-lib/src/public-api.ts
+++ projects/ui-lib/src/public-api.ts
@@ -2,19 +2,19 @@
  * Public API Surface of ui-lib
  */
 
-export * as BlakFillerPipe from './lib/pipes/blank-filler.pipe';
-export * as SnakeCaseParserPipe from './lib/pipes/snake-case-parser.pipe';
-export * as FieldsMatchValidatorDirective from './lib/directives/fields-match-validator.directive';
-export * as PhoneValidationDirective from './lib/directives/phone-validation.directive';
-export * as DefaultDialogComponent from './lib/components/default-dialog/default-dialog.component';
-export * as ErrorDispalyComponent from './lib/components/error-dispaly.component';
-export * as SearchBarComponent from './lib/components/search-bar.component';
+export * from './lib/pipes/blank-filler.pipe';
+export * from './lib/pipes/snake-case-parser.pipe';
+export * from './lib/directives/fields-match-validator.directive';
+export * from './lib/directives/phone-validation.directive';
+export * from './lib/components/default-dialog/default-dialog.component';
+export * from './lib/components/error-dispaly.component';
+export * from './lib/components/search-bar.component';
 export * from './lib/utils/types';
 export * from './lib/utils/animations';
-export * as TableSortHeaderComponent from './lib/components/table-sort-header/table-sort-header.component';
-export * as TableComponent from './lib/components/table/table.component';
-export * as GlobalLoaderComponent from './lib/components/global-loader/global-loader.component';
-export * as LoaderService from './lib/services/loader.service';
-export * as ErrorService from './lib/services/error.service';
-export * as ErrorHandlerComponent from './lib/components/error-handler/error-handler.component';
-export * as ChipComponent from './lib/components/chip/chip.component';
+export * from './lib/components/table-sort-header/table-sort-header.component';
+export * from './lib/components/table/table.component';
+export * from './lib/components/global-loader/global-loader.component';
+export * from './lib/services/loader.service';
+export * from './lib/services/error.service';
+export * from './lib/components/error-handler/error-handler.component';
+export * from './lib/components/chip/chip.component';
