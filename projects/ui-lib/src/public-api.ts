237f1b05a1706b58fe2794290703d57b04ac33d2
Ferhan Cherkez <fericherkez14052002@gmail.com>
2025-02-20 21:08:57 +0200

    adds password validator directive to public api

--- projects/ui-lib/src/public-api.ts
+++ projects/ui-lib/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/pipes/blank-filler.pipe';
 export * from './lib/pipes/snake-case-parser.pipe';
 export * from './lib/directives/fields-match-validator.directive';
 export * from './lib/directives/phone-validation.directive';
+export * from './lib/directives/password-validator.directive';
 export * from './lib/components/default-dialog/default-dialog.component';
 export * from './lib/components/error-dispaly.component';
 export * from './lib/components/search-bar.component';
