aec0a85d61b33a30d4f513dbdb1360da1f90962d
Ferhan Cherkez <fericherkez14052002@gmail.com>
2025-10-21 13:08:12 +0300

    fixes snake case pipe behavior for non string inputs

--- projects/ui-lib/src/lib/pipes/snake-case-parser.pipe.ts
+++ projects/ui-lib/src/lib/pipes/snake-case-parser.pipe.ts
@@ -4,12 +4,16 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'snakeCaseParser',
 })
 export class SnakeCaseParserPipe implements PipeTransform {
-  public transform(str: string | undefined): string {
-    if (str) {
-      let temp = str.split('_').map(word => {
-        return word.toLowerCase();
-      }).join(' ');
-      return temp.charAt(0).toUpperCase() + temp.slice(1);
+  public transform(value: string | undefined): string {
+    if (value) {
+      if (typeof value === 'string') {
+        let temp = value.split('_').map(word => {
+          return word.toLowerCase();
+        }).join(' ');
+        return temp.charAt(0).toUpperCase() + temp.slice(1);
+      }
+
+      return String(value);
     }
 
     return '';
